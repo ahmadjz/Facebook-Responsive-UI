480e19c1fce5d0fd2368853ec37b08307bbc8d6f
Ahmad jz <ahmadjzz99@gmail.com>
2022-12-12 14:42:00 +0300

    create rooms section

---
+++ lib/screens/home/widgets/rooms.dart
@@ -0,0 +1,80 @@
+import 'package:facebook_responsive_ui/config/palette.dart';
+import 'package:facebook_responsive_ui/models/models.dart';
+import 'package:facebook_responsive_ui/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+
+class Rooms extends StatelessWidget {
+  final List<User> onlineUsers;
+
+  const Rooms({
+    Key? key,
+    required this.onlineUsers,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 60,
+      color: Colors.white,
+      child: ListView.builder(
+        padding: const EdgeInsets.symmetric(
+          vertical: 10.0,
+          horizontal: 4.0,
+        ),
+        scrollDirection: Axis.horizontal,
+        itemCount: 1 + onlineUsers.length,
+        itemBuilder: (BuildContext context, int index) {
+          if (index == 0) {
+            return Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8.0),
+              child: _CreateRoomButton(),
+            );
+          }
+          final User user = onlineUsers[index - 1];
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: ProfileAvatar(
+              imageUrl: user.imageUrl,
+              isActive: true,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _CreateRoomButton extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return OutlinedButton(
+      onPressed: () {},
+      style: OutlinedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        side: BorderSide(
+          width: 3.0,
+          color: Colors.blueAccent[100]!,
+        ),
+        textStyle: const TextStyle(color: Palette.facebookBlue),
+        backgroundColor: Colors.white,
+      ),
+      child: Row(
+        children: [
+          ShaderMask(
+            shaderCallback: (rect) =>
+                Palette.createRoomGradient.createShader(rect),
+            child: const Icon(
+              Icons.video_call,
+              size: 35.0,
+              color: Colors.purple,
+            ),
+          ),
+          const SizedBox(width: 4.0),
+          const Text('Create\nRoom'),
+        ],
+      ),
+    );
+  }
+}
--- lib/screens/home/widgets/widgets.dart
+++ lib/screens/home/widgets/widgets.dart
@@ -1,1 +1,2 @@
 export 'create_post_container.dart';
+export 'rooms.dart';

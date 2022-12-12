f5e2fb4f0d180f3c1e56a9b558f380a2c2925a10
Ahmad jz <ahmadjzz99@gmail.com>
2022-12-12 20:09:15 +0300

    create bottom bar

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:facebook_responsive_ui/config/palette.dart';
-import 'package:facebook_responsive_ui/screens/screens.dart';
+import 'package:facebook_responsive_ui/screens/home/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,14 +13,97 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    PersistentTabController controller =
+        PersistentTabController(initialIndex: 0);
+    List<Widget> _buildScreens() {
+      return [
+        const HomeScreen(),
+        const Scaffold(),
+        const Scaffold(),
+        const Scaffold(),
+        const Scaffold(),
+        const Scaffold()
+      ];
+    }
+
+    List<PersistentBottomNavBarItem> _navBarsItems() {
+      return [
+        PersistentBottomNavBarItem(
+          icon: const Icon(Icons.home),
+          inactiveColorPrimary: Colors.grey,
+        ),
+        PersistentBottomNavBarItem(
+          icon: const Icon(Icons.ondemand_video),
+          inactiveColorPrimary: Colors.grey,
+        ),
+        PersistentBottomNavBarItem(
+          icon: const Icon(
+            MdiIcons.accountCircleOutline,
+          ),
+          inactiveColorPrimary: Colors.grey,
+        ),
+        PersistentBottomNavBarItem(
+          icon: const Icon(
+            MdiIcons.accountGroupOutline,
+          ),
+          inactiveColorPrimary: Colors.grey,
+        ),
+        PersistentBottomNavBarItem(
+          icon: const Icon(
+            MdiIcons.bellOutline,
+          ),
+          inactiveColorPrimary: Colors.grey,
+        ),
+        PersistentBottomNavBarItem(
+          icon: const Icon(
+            Icons.menu,
+          ),
+          inactiveColorPrimary: Colors.grey,
+        ),
+      ];
+    }
+
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: "Flutter Facebook UI",
-      theme: ThemeData(
-          primarySwatch: Colors.blue,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-          scaffoldBackgroundColor: Palette.scaffold),
-      home: const HomeScreen(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: "Flutter Facebook UI",
+        theme: ThemeData(
+            primarySwatch: Colors.blue,
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+            scaffoldBackgroundColor: Palette.scaffold),
+        home: Builder(builder: (context) {
+          return PersistentTabView(
+            context,
+            controller: controller,
+            screens: _buildScreens(),
+            items: _navBarsItems(),
+            confineInSafeArea: true,
+            backgroundColor: Colors.white, // Default is Colors.white.
+            handleAndroidBackButtonPress: true, // Default is true.
+            resizeToAvoidBottomInset:
+                true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+            stateManagement: true, // Default is true.
+            hideNavigationBarWhenKeyboardShows:
+                true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+            decoration: NavBarDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              colorBehindNavBar: Colors.white,
+            ),
+            popAllScreensOnTapOfSelectedTab: true,
+            popActionScreens: PopActionScreensType.all,
+            itemAnimationProperties: const ItemAnimationProperties(
+              // Navigation Bar's items animation properties.
+              duration: Duration(milliseconds: 200),
+              curve: Curves.ease,
+            ),
+            screenTransitionAnimation: const ScreenTransitionAnimation(
+              // Screen transition animation on change of selected tab.
+              animateTabTransition: true,
+              curve: Curves.ease,
+              duration: Duration(milliseconds: 200),
+            ),
+            navBarStyle: NavBarStyle
+                .style3, // Choose the nav bar style with this property.
+          );
+        }));
   }
 }
